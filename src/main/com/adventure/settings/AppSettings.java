812cc91b24abf0b4b8450c8decc313b32e815676
Santiago Coria <santiago_coria@hotmail.com>
2024-02-01 20:45:39 -0300

    Mastery Task1

--- src/main/com/adventure/settings/AppSettings.java
+++ src/main/com/adventure/settings/AppSettings.java
@@ -3,7 +3,7 @@ package main.com.adventure.settings;
 public class AppSettings {
 
     /** You will update this property for each project. **/
-    public static final Story story = Story.Invalid_Beginning;
+    public static final Story story = Story.MT1_Beginning;
 
     public enum Story {
         TestScene,
